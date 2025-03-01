4f8dd60c27867e7290b8de36f00968eb8562b878
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-03-01 17:05:07 +0100

    update

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyautogui
+from PIL import Image 
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+
+# im = Image.open("dinosaur.jpg")
+# im.show()
+@app.route('/')
+def home():
+    return render_template("game.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#display the image 
+#make the character move 
+#

8df5f838f7da3fe74d43ec1ea567472f52a87ab6
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-03-03 10:36:04 +0100

    update

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# im = Image.open("dinosaur.jpg")
-# im.show()
 @app.route('/')
 def home():
     return render_template("game.html")
